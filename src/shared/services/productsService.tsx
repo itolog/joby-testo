419592ef5fc18d1b89ec1c24b3a6d3e6f562513f
itolog <paukanlove@gmail.com>
2019-07-13 19:47:12 +0300

    bugggy fix

--- src/shared/services/productsService.tsx
+++ src/shared/services/productsService.tsx
@@ -1,22 +1,5 @@
 import axios from 'axios';
 import { Observable } from 'rxjs';
-<<<<<<< HEAD
-=======
-
-// export default class ProductsService {
-//   static getProducts() {
-//     return new Promise((resolve, reject) => {
-//       axios.get('http://www.mocky.io/v2/5d21cc652f00006f2cc46338')
-//         .then(({data}: any) => {
-//           resolve(data.products);
-//         })
-//         .catch(e => {
-//           reject(e.message);
-//         });
-//     })
-//   }
-// }
->>>>>>> ce3e446864fbe2d00cd0f77c941cd356c76c02c2
 
 export default class ProductsService {
   static getProducts() {
